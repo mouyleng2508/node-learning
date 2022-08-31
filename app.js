407e2364dc494f8ae646a8761bb6edd2889ab712
Momoleng <mouyleng@Momolengs-MacBook-Pro.local>
2022-08-31 12:48:02 +0700

    file system module

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const os = require("os")
+const fs = require('fs')
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// var files = fs.readFileSync("./")
+// console.log(files);
 
-console.log("Total Memory" + totalMemory);
-console.log("Free Memory" + freeMemory)
-
-//Template string
-//ES6 / ES2015: ECMA6 
-
-console.log(`Total Memory ${totalMemory}`)
-console.log(`Free Memory ${freeMemory}`)
+fs.readdir('./', function (err, files) {
+    if (err) console.log("Error", err);
+    else console.log("Result", files)
+})
